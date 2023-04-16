9f8cbfebdb9334f10eaa54036b61d12d5221d630
rahul-rautwar <rahulrautwar@gmail.com>
2023-04-16 14:24:26 +0530

    add homepage profile component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Profile } from "./components/Home/Profile";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home/Profile.js
@@ -0,0 +1,72 @@
+import React from "react";
+import Typical from "react-typical";
+// import Typical from "react-typical";
+import "./Profile.css";
+export const Profile = () => {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icons">
+              <a href="https://github.com/rahul-rautwar">
+                <i className="fa fa-github"></i>
+              </a>
+              <a href="https://github.com/rahul-rautwar">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="https://github.com/rahul-rautwar">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="https://github.com/rahul-rautwar">
+                <i className="fa fa-youtube-square"></i>
+              </a>
+              <a href="https://github.com/rahul-rautwar">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'm <span className="highlighted-text">Rahul</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1 className="">
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Senior Frontend Developer 🧑‍💻",
+                    "1000",
+                    "React Developer 💻",
+                    "1000",
+                    "Angular Web Developer 📱",
+                    "1000",
+                    "Web Developer 🌐",
+                    "1000",
+                  ]}
+                ></Typical>
+              </h1>
+              <span className="profile-role-tagline">
+                I'm a Front-End Software Developer by passion and who loves
+                creative coding.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">Hire Me!!</button>
+            <a href="Rahul_Rautwar_Resume.pdf" download="Rahul Rautwar Resume">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
