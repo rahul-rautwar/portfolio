2c2c71af42f179130f9eafa14c3094a24a182421
rahul-rautwar <rahulrautwar@gmail.com>
2023-04-18 18:14:24 +0530

    add header component

---
+++ src/components/Home/Header.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Header.css";
+import { useState } from "react";
+export const Header = () => {
+  const [showHeaderOptions, setShowHeaderOptions] = useState(0);
+  const pages = [
+    {
+      name: "Home",
+    },
+    {
+      name: "AboutMe",
+    },
+    {
+      name: "Resume",
+    },
+    {
+      name: "Skills",
+    },
+    {
+      name: "Projects",
+    },
+    {
+      name: "ContactMe",
+    },
+  ];
+  return (
+    <div
+      className="header-container"
+      onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+    >
+      <div className="header-parent">
+        <div
+          className="header-hamburger"
+          onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+        >
+          <FontAwesomeIcon
+            className="header-hamburger-bars"
+            icon={faBars}
+          ></FontAwesomeIcon>
+        </div>
+        <div className="header-logo">
+          {/* <span>Logo here</span> */}
+          {/* <img src="" /> */}
+        </div>
+        <div
+          className={
+            showHeaderOptions
+              ? "header-options show-hamburger-options"
+              : "header-options"
+          }
+        >
+          {pages.map((page, i) => (
+            <div
+              key={page.name}
+              className={`header-option header-option-separator `}
+            >
+              <span>{page.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Footer } from "./Footer";
 import { Profile } from "./Profile";
 import "./Home.css";
+import { Header } from "./Header";
 
 export const Home = () => {
   return (
     <div className="home-container">
+      <Header />
       <Profile />
       <Footer />
     </div>
