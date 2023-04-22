ab55fd6341c830a37ce1ffb1fc9fc7b928fbcb9f
rahul-rautwar <rahulrautwar@gmail.com>
2023-04-22 14:35:36 +0530

    add skills component

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,156 @@
+import React from "react";
+import "./Skills.css";
+
+import ReactLogo from "../../assets/img/icons/react.png";
+import AngularLogo from "../../assets/img/icons/angular.png";
+import VueLogo from "../../assets/img/icons/vue.png";
+import JSLogo from "../../assets/img/icons/js.png";
+import TSLogo from "../../assets/img/icons/ts.png";
+import ReduxLogo from "../../assets/img/icons/redux.png";
+import HtmlLogo from "../../assets/img/icons/html.png";
+import CssLogo from "../../assets/img/icons/css.png";
+import SassLogo from "../../assets/img/icons/sass.png";
+import JestLogo from "../../assets/img/icons/jest.png";
+import GitLogo from "../../assets/img/icons/git.png";
+import NodeLogo from "../../assets/img/icons/node.png";
+import TailwindLogo from "../../assets/img/icons/tailwind.png";
+import BootstrapLogo from "../../assets/img/icons/bootstrap.png";
+import StencilLogo from "../../assets/img/icons/stencil.png";
+
+export const Skills = () => {
+  return (
+    <div className="py-[75px] sm:py-[100px]">
+      <div className="flex flex-col w-full max-w-screen-xl mx-auto px-5">
+        <h1
+          id="Skills"
+          className="font-mono mb-4 text-center font-medium text-2xl sm:text-3xl lg:text-4xl"
+        >
+          Skills
+        </h1>
+        <div className="flex flex-wrap justify-center w-full gap-3 sm:gap-0">
+          <a
+            href="https://angular.io/"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+            title="Angular"
+          >
+            <img src={AngularLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={ReactLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={VueLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={JSLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={TSLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={ReduxLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={HtmlLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={CssLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={SassLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={JestLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={GitLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={NodeLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={TailwindLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={BootstrapLogo} alt={` TypeScript logo`} />
+          </a>
+          <a
+            href="ss"
+            target="_blank"
+            class="w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] flex justify-center flex-col sm:m-4 p-6 border-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition duration-[0.3s] rounded-[5px]"
+            rel="noreferrer"
+          >
+            <img src={StencilLogo} alt={` TypeScript logo`} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
