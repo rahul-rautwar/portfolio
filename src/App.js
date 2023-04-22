c5229d61529285a65dd957b103b22413c21538fc
rahul-rautwar <rahulrautwar@gmail.com>
2023-04-22 14:42:32 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import { AboutMe } from "./components/AboutMe/AboutMe";
 import Contact from "./components/Contact/Contact";
 import { Home } from "./components/Home/Home";
+import { Projects } from "./components/Projects/Projects";
+import { Skills } from "./components/Skills/Skills";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <AboutMe />
+      <Skills />
+      <Projects />
       <Contact />
     </div>
   );
